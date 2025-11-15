744939a1a3d19be520fbcd561bb9b1443fde4317
benjamin <124478194+PaschBnj@users.noreply.github.com>
2025-11-15 14:01:06 -0300

    Update cadastro.py

--- Nova pasta (2)/cadastro.py
+++ Nova pasta (2)/cadastro.py
@@ -11,24 +11,26 @@ conn = pymysql.connect(
 
 cursor = conn.cursor()
 
-def cadastrar_usuario(nome_usuario, senha):
+def cadastrar_usuario(nome_usuario, senha, cargo, numero_telefone):
     salt = bcrypt.gensalt()
     senha_hash = bcrypt.hashpw(senha.encode('utf-8'), salt)
     
     cursor.execute("""
-        INSERT INTO usuarios (nome_usuario, senha)
-        VALUES (%s, %s)                 
-    """, (nome_usuario, senha_hash))
+        INSERT INTO usuarios (nome_usuario, senha, cargo, numero_telefone)
+        VALUES (%s, %s, %s, %s)                 
+    """, (nome_usuario, senha_hash, cargo, numero_telefone))
     conn.commit()
     print("Usuário cadastrado com sucesso!")
 
     
 
 while True:
-    nome_usuario = input("Digite seu nome de usuário: ")
+    nome_usuario = input("Digite o nome de usuário a ser cadastrado: ")
     senha = input("Digite sua senha: ")
+    cargo = input("Qual o cargo do usuario a ser cadastrado? (cliente/funcionario/gerente)")
+    numero_telefone = input("Qual o numero de contato(telefone) do usuario a ser cadastrado?")
     
-    cadastrar_usuario(nome_usuario, senha)
+    cadastrar_usuario(nome_usuario, senha, cargo, numero_telefone)
     
     continuar = input("Deseja cadastrar outro usuário? (s/n): ")
     if continuar.lower() != 's':
